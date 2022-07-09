1a98a297e36cb0b0019d678322fceb0b4fe15a7b
Sampath Kumar <sampath.kumar@mail.waldenu.edu>
2022-07-09 16:47:05 +0530

    Wiping up

--- routes/userRoute.js
+++
@@ -1,8 +0,0 @@
-const routes = require('express').Router();
-const userValidator  = require('../middlewares/validation/userValidator');
-const validateErrors = require('../middlewares/validation/validateErrors').validateErrors;
-const userController = require('../controllers/userController');
-
-routes.post('/register', userValidator.registerUserValidationRules(), validateErrors, userController.registerUser);
-routes.post('/login',    userValidator.loginUserValidationRules(), validateErrors, userController.loginUser);
-module.exports = routes; 
