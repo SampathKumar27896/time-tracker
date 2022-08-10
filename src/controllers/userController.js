4d5b92cf9a7ffa4cf890116fc2f9c1399229fd45
Sampath Kumar <sampathkumar27896@hotmail.com>
2022-08-10 22:52:45 +0530

    Added basic validation for user registeration

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,11 +1,20 @@
 const {User} = require('../mongoSchemas/schema');
 const registerUser = (req, res) => {
         try {
-                console.log(req.body);
-                const { userName, password, emailId } = req.body;
-                const user = new User({name: userName, password: password, emailId: emailId})
-                user.save();
-                res.send(user);
+                let status = true, message = 'Registeration successful';
+                const { userName, password, confirmPassword,  emailId } = req.body;
+                if(password !== confirmPassword) {
+                        status = false;
+                        message = `Re-Entered password don't match with password!.`;
+                }
+                if(userName && password && confirmPassword &&  emailId) {
+                        const user = new User({name: userName, password: password, emailId: emailId})
+                        user.save();
+                } else {
+                        status = false;
+                        message = `Validation failed!.`;
+                }
+                res.send({status, message});
         }catch(error) {
                 console.log(error);
                 throw new error;
